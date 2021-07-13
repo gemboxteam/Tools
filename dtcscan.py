ea9bc6b596bec1602865ed212c546ef7af58bd34
NinjaBuster <erwan182@gmail.com>
2021-07-14 06:02:54 +0700

    Update dtcscan.py

--- dtcscan.py
+++ dtcscan.py
@@ -1,3 +1,4 @@
+
 import os
 import readline
 import requests
@@ -37,7 +38,7 @@ print("   #     #    #    #             # #       ####### #   # #")
 print("   #     #    #    #     # #     # #     # #     # #    ##")
 print("   ######     #     #####   #####   #####  #     # #     #")
 print("")
-print("                                       Domain Enumerations")
+prYellow(" *============ Massive Domain Enumerations ==============*")
 print("")
 
 
@@ -46,7 +47,7 @@ url = raw_input("Enter Target Url : ")
 print("Enter Target Url : " + (url))
 os.system('clear')
 
-a = "python3 domain-enum.py -p22,80,443,21,53,3306,445 -o domain.txt -d" + (url)
+a = "python3 domain-enum.py -p21,22,23,25,53,67,80,110,111,123,139,143,443,445,465,3000,3306,3389,5900,631,993,995,8008,8080,8081,9001 -o domain.txt -d" + (url)
 os.system(a)
 
 
@@ -105,13 +106,11 @@ uniqlines = open('output.txt', 'w').writelines(set(uniqlines))
 print("")
 
 os.remove("domain-enum.py")
-
 time.sleep(2)
-print("")
 
-prGreen("=============================================================================================")
-prYellow("[+] Total Digital Assets Findings for " + (url) + " by Massive Domains Enumeration :   ")
-prGreen("=============================================================================================")
+prGreen("==================================================================================================")
+prYellow("[+] Total Digital Asset Finding for " + (url) + " by Valid Massive Domain & IP  Enumeration:   ")
+prGreen("==================================================================================================")
 print("")
 
 # read file and print output
@@ -142,15 +141,58 @@ prYellow("   ===> Valid ip Saved as valid-ip.txt")
 prYellow("   ===> Valid domains Saved as valid-domains.txt")
 print("")
 
-#Get ASN Number
-prGreen("=========================================================================")
-prYellow("[+] Total Digital Assets Findings for " + (url) + " by AS Number :   ")
-prGreen("=========================================================================")
-
-
-print("")
+#Get ip for ASN
 inasn = open('data.txt', 'w')
 getasn = socket.gethostbyname (url)
-
 os.system("service tor restart")
+print("")
 
+
+
+prGreen("==============================================================================")
+prYellow("[+] ASN Lookup For " + (url) + ": ")
+prGreen("==============================================================================")
+
+
+findasn = "proxychains curl https://api.hackertarget.com/aslookup/?q=" + (getasn) + " > preasn.txt" + " 2>/dev/null"
+os.system(findasn)
+
+#nano = open("preasn.txt", "r")
+#print(nano.read())
+#nano.close()
+
+#Parsing just ASN
+assn = "cat preasn.txt |cut -d ',' -f 2 |sed 1d | tail -c +2 | head -c -2  > preasn2.txt"
+os.system(assn)
+
+#Remove Quotes
+fin = open("preasn2.txt", "rt")
+fout = open("asn.txt", "wt")
+
+for line in fin:
+    fout.write(line.replace('"', ''))
+
+fin.close()
+fout.close()
+print("")
+
+who = "whois -h whois.cymru.com " + (getasn)
+os.system(who)
+print("")
+
+prGreen("==============================================================================")
+prYellow("[+] Total Digital Asset Finding for " + (url) + " by Massive Enumeration:   ")
+prGreen("==============================================================================")
+
+
+#GET CIDR from ASN
+cidrr =  open('asn.txt', 'r')
+cid = cidrr.read()
+cidr = "proxychains curl https://api.hackertarget.com/aslookup/?q=AS" + (cid) + " > cidr.txt" + " 2>/dev/null"
+os.system(cidr)
+print("")
+
+assn2 = "cat cidr.txt |sed -e '1,2d' > cidr2.txt"
+os.system(assn2)
+os.system("cat cidr2.txt")
+print("")
